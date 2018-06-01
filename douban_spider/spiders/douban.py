218898d728365dcb4ec2932f49c379cb52f7434e
tianming <tianming@qunhemail.com>
2018-06-01 20:07:55 +0800

    add

--- douban_spider/spiders/douban.py
+++ douban_spider/spiders/douban.py
@@ -16,27 +16,27 @@ class DouBanSpider(scrapy.Spider):
     name = "douban"
     allowed_domains = ["www.douban.com"]
     start_urls = [
-        "https://book.douban.com/tag/互联网/"
+        "https://book.douban.com/tag/算法/"
     ]
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
     }
 
     def start_requests(self):
-        url = "https://book.douban.com/tag/互联网"
+        url = "https://book.douban.com/tag/算法/"
         yield Request(url, headers=self.headers)
 
     def parse(self, response):
         for book in response.xpath('//ul[@class = "subject-list"]/li[@class="subject-item"]'):
             item = DouBanBookItem()
-            name1 = book.xpath('.//div[@class="info"]/h2/a/text()').extract_first().rstrip('\n').lstrip('\n').strip()
+            name1 = book.xpath('.//div[@class="info"]/h2/a/text()').extract_first(default='').rstrip('\n').lstrip('\n').strip()
             name2 = book.xpath('.//div[@class="info"]/h2/a/span/text()').extract_first(default='').rstrip('\n').lstrip('\n').strip()
             item['name'] = name1 + name2
-            item['pub'] = book.xpath('.//div[@class="info"]/div[@class="pub"]/text()').extract_first().rstrip('\n').lstrip('\n').strip()
+            item['pub'] = book.xpath('.//div[@class="info"]/div[@class="pub"]/text()').extract_first(default='').rstrip('\n').lstrip('\n').strip()
             item['star'] = book.xpath('.//div[@class="info"]/div[@class="star clearfix"]/span[@class="rating_nums"]/text()').\
-                extract_first().rstrip('\n').lstrip('\n').strip()
-            item['desc'] = book.xpath('.//div[@class="info"]/p/text()').extract_first().rstrip('\n').lstrip('\n').strip()
-            item['category'] = response.xpath('//div[@id="content"]/h1/text()').extract_first()
+                extract_first(default=0)
+            item['desc'] = book.xpath('.//div[@class="info"]/p/text()').extract_first(default='').rstrip('\n').lstrip('\n').strip()
+            item['category'] = response.xpath('//div[@id="content"]/h1/text()').extract_first(default='')
             yield item
 
         for url in response.xpath('//div[@class="paginator"]/a/@href').extract():
